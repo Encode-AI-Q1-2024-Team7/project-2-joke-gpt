885e89decad6a7c4a6dbdb6c0b347102016d33cb
Chris Salvador <csalvador58@gmail.com>
2024-03-29 19:59:34 -0700

    setup layout ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,41 @@
 'use client';
 
+import DropDownBox from '@/components/ui/dropdown';
 import { useChat } from 'ai/react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Option } from '@/components/ui/dropdown';
+
+const Topics: Option[] = [
+  { id: 0, name: 'Select...' },
+  { id: 1, name: 'People' },
+  { id: 2, name: 'Jobs' },
+  { id: 3, name: 'SciFi' },
+  { id: 4, name: 'Animals' },
+  // More topics...
+];
+
+const Tones: Option[] = [
+  { id: 0, name: 'Select...' },
+  { id: 1, name: 'Witty' },
+  { id: 2, name: 'Boring' },
+  { id: 3, name: 'Dark' },
+  { id: 4, name: 'Rushed' },
+  // More tones...
+];
+
+const Types: Option[] = [
+  { id: 0, name: 'Select...' },
+  { id: 1, name: 'Story' },
+  { id: 2, name: 'Knock Knock' },
+  { id: 3, name: 'Poetic' },
+  { id: 4, name: 'Country' },
+  // More types...
+];
 
 export default function Chat() {
+  const [selectedTopic, setSelectedTopic] = useState(Topics[0]);
+  const [selectedTone, setSelectedTone] = useState(Tones[0]);
+  const [selectedType, setSelectedType] = useState(Types[0]);
   const {
     messages,
     input,
@@ -28,8 +60,9 @@ export default function Chat() {
         Joker GPT
       </header>
       <div className='flex min-w-lg'>
-        <div className='bg-slate-500 relative w-[500px]'>
-          <div className='fixed bg-slate-900 rounded-md'>
+        <div className='relative min-w-[500px]'>
+          <h2 className='text-2xl font-bold text-center mb-4'>GPT Settings</h2>
+          <div className='sticky top-36 bg-slate-900 rounded-md mx-3 py-4'>
             <div className='flex flex-col justify-center mb-2 items-center p-4'>
               <button
                 className='bg-blue-500 p-2 text-white rounded shadow-xl'
@@ -41,7 +74,31 @@ export default function Chat() {
                 Generate a Joke
               </button>
             </div>
-            <form onSubmit={handleSubmit} className='flex justify-center'>
+            <div className='flex justify-center py-2'>
+              <DropDownBox
+                state={selectedTopic}
+                setState={setSelectedTopic}
+                title='topic'
+                options={Topics}
+              />
+            </div>
+            <div className='flex justify-center py-2'>
+              <DropDownBox
+                state={selectedTone}
+                setState={setSelectedTone}
+                title='tone'
+                options={Tones}
+              />
+            </div>
+            <div className='flex justify-center py-2'>
+              <DropDownBox
+                state={selectedType}
+                setState={setSelectedType}
+                title='joke type'
+                options={Tones}
+              />
+            </div>
+            {/* <form onSubmit={handleSubmit} className='flex justify-center'>
               <input
                 className='w-[95%] p-2 mb-8 border border-gray-300 rounded shadow-xl text-black'
                 disabled={isLoading}
@@ -49,10 +106,11 @@ export default function Chat() {
                 placeholder='Say something...'
                 onChange={handleInputChange}
               />
-            </form>
+            </form> */}
           </div>
         </div>
         <div className='flex flex-col w-full h-5/6 min-w-96 max-w-xl mx-auto'>
+          <h2 className='text-2xl font-bold text-center mb-4'>Chat</h2>
           <div className='overflow-auto mb-8 w-full' ref={messagesContainerRef}>
             {messages.map((m) => (
               <div
